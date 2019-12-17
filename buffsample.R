daf87ffcfe9d93ab790de89ab60007dfdf7b95f7
Benedikt Arnthof <ru25jan@rstudio3.srv.lrz.de>
2019-12-17 10:28:04 +0100

    add buffsample file

---
+++ buffsample.R
@@ -0,0 +1,143 @@
+# I'm only interested in settlements from the iron age
+raster::unique(raw_data$Epoche)
+presence <- raw_data[raw_data$Epoche %in% c("Hallstattzeit", "Latènezeit"),]
+nrow(presence)
+sites <- dplyr::select(presence, lng_wgs84, lat_wgs84)
+head(sites)
+# selecting unique rows to combat biased models
+sites <- unique(sites[,c("lng_wgs84", "lat_wgs84")])
+# 6206 sites remaining
+# creating buffers around points to sample nonsites correctly
+
+temp <- sampleRandom(predictors[[1]], 1000, sp = T)
+sp_sites <- sp::SpatialPoints(coords      = sites[,c("lng_wgs84","lat_wgs84")], # order matters
+                              proj4string = predictors@crs)
+utm_sites <- spTransform(sp_sites, CRS("+proj=utm +zone=32 ellps=WGS84"))
+sf_sites <- st_as_sf(utm_sites)
+
+sites_buff_1000m <- st_buffer(sf_sites, dist = 1000)
+temp_utm <- spTransform(temp, CRS("+proj=utm +zone=32 ellps=WGS84"))
+
+tmp <- temp_utm[sites_buff_1000m$geometry,]
+
+sp_polygons_buffer <- sf::as_Spatial(sites_buff_1000m$geometry)
+crs(sp_polygons_buffer) <- crs(temp_utm)
+over(temp_utm, sp_polygons_buffer)
+nrow(temp_utm)
+ret <- temp_utm[!is.na(over(temp_utm, sp_polygons_buffer)),]
+# this returns the points that fall within the buffer zone. 
+# lets try to increase the radius
+sites_buff_2500m <- st_buffer(sf_sites, dist = 2500)
+sp_polygons_2500 <- sf::as_Spatial(sites_buff_2500m$geometry)
+crs(sp_polygons_2500) <- crs(temp_utm)
+ret2500_within <- temp_utm[!is.na(over(temp_utm, sp_polygons_2500)),]
+nrow(ret2500_within)
+ret2500_within <- spTransform(ret2500_within, crs(temp))
+ret2500_without <- temp_utm[is.na(over(temp_utm, sp_polygons_2500)),]
+ret2500_without <- spTransform(ret2500_without, crs(temp))
+
+# mapview(sp_sites) + 
+#   mapview(ret2500_without, color = "red")
+# # works as expected 
+
+# function to sample around the points with a given buffer
+buffsample <- function(ssize = 1000, distance = 1000, within = FALSE, returnsize = 1000) {
+  ret <- new("SpatialPoints",                                                          
+             coords = structure(numeric(0), .Dim = c(0L, 2L),                          
+                                .Dimnames = list(NULL, c("coords.x1", "coords.x2"))),  
+             bbox = structure(c(1, 1, 1, 1), .Dim = c(2L, 2L),                         
+                              .Dimnames = list(c("coords.x1", "coords.x2"),
+                                               c("min", "max"))),
+             proj4string = new("CRS", projargs = "+proj=utm +zone=32 ellps=WGS84 +ellps=WGS84"))
+  sp_sites <- sp::SpatialPoints(coords = sites[,c("lng_wgs84","lat_wgs84")], proj4string = predictors@crs)
+  utm_sites <- spTransform(sp_sites, CRS("+proj=utm +zone=32 ellps=WGS84"))
+  sf_sites <- st_as_sf(utm_sites)
+  sites_buff <- st_buffer(sf_sites, dist = distance)
+  
+  sp_polygons_buffer <- sf::as_Spatial(sites_buff$geometry)
+  while (nrow(ret@coords) < returnsize) {
+    sample <- sampleRandom(predictors[[1]], ssize, sp = T)
+    sample_utm <- spTransform(sample, CRS("+proj=utm +zone=32 ellps=WGS84"))
+    crs(sp_polygons_buffer) <- crs(sample_utm)
+    over(sample_utm, sp_polygons_buffer)
+    nrow(sample_utm)
+    if (within == TRUE) {
+      tmp <- sample_utm[!is.na(over(sample_utm, sp_polygons_buffer)),]
+    } else {
+      tmp <- sample_utm[is.na(over(sample_utm, sp_polygons_buffer)),]
+    }
+    ret <- maptools::spRbind(ret, tmp)
+  }
+  ret <- spTransform(ret, crs(sp_sites))
+  ret <- ret[1:returnsize,]
+  ret <- coordinates(ret)
+  colnames(ret) <- c("lng_wgs84", "lat_wgs84")
+  ret <- as.data.frame.matrix(ret)
+  return(ret)
+}
+set.seed(123)
+funtest <- buffsample(ssize = 2000, distance = 1500, returnsize = 10000)
+funtest5k <- buffsample(ssize = 2000, distance = 1500, returnsize = 5000)
+sites_buff_1500m <- st_buffer(sf_sites, dist = 1500)
+nrow(funtest)
+
+coordinates(funtest5k) <- c("lng_wgs84", "lat_wgs84")
+mapview(sites_buff_1500m) + 
+  mapview(funtest5k, alpha.regions = 0.1, color = "red")
+# works as intended and performs reasonably well
+# finalizing evidence
+
+# function to automate generation of evidence data
+generateEvidence <- function(sitesdata, nonsitesdata, predictorstack = predictors) {
+  # selecting site points
+  sites_temp <- sitesdata
+  sites_temp$lon <- as.numeric(as.vector(sites_temp$lng_wgs84))
+  sites_temp$lat <- as.numeric(as.vector(sites_temp$lat_wgs84))
+  # convert to spatial data in order to extract the predictor values for all points
+  coordinates(sites_temp) <- c("lon","lat")
+  proj4string(sites_temp) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+  nonsites_temp <- nonsitesdata
+  nonsites_temp$lon <- as.numeric(as.vector(nonsites_temp$lng_wgs84))
+  nonsites_temp$lat <- as.numeric(as.vector(nonsites_temp$lat_wgs84))
+  coordinates(nonsites_temp) <- c("lon", "lat")
+  proj4string(nonsites_temp) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+  # extracting predictor values for sites and nonsites
+  sSP <- SpatialPoints(sites_temp@coords)
+  nsSP <- SpatialPoints(nonsites_temp@coords)
+  values_sites <- raster::extract(predictorstack, sSP)
+  values_nonsites <- raster::extract(predictorstack, nsSP)
+  # converting back to data.frame for modeling
+  coords_sites <- sites_temp@coords
+  coords_sites <- as.data.frame(coords_sites)
+  coords_nonsites <- nonsites_temp@coords
+  coords_nonsites <- as.data.frame(coords_nonsites)
+  values_sites <- as.data.frame(values_sites)
+  values_nonsites <- as.data.frame(values_nonsites)
+  values_sites$site <- 1
+  values_nonsites$site <- 0
+  values_sites$lon <- coords_sites$lon
+  values_sites$lat <- coords_sites$lat
+  values_nonsites$lon <- coords_nonsites$lon
+  values_nonsites$lat <- coords_nonsites$lat
+  evidence <- rbind(values_sites, values_nonsites)
+  evidence <- na.omit(evidence)
+  return(evidence)
+}
+
+# function to draw sets of equal size
+finalizeEvidence <- function(evd){
+  siedl_pts <- filter(evd, site == 1)
+  nons_pts <- filter(evd, site == 0)
+  sz <- nrow(siedl_pts)
+  nons_pts_sub <- sample_n(nons_pts, size = sz)
+  temp <- rbind(siedl_pts, nons_pts_sub)
+  return(temp)
+}
+
+evidence <- generateEvidence(sitesdata = sites, nonsitesdata = funtest, predictorstack = predictors)
+evidence <- finalizeEvidence(evidence)
+
+saveRDS(evidence, file = "Daten/evidence.csv")
+evidence <- readRDS("Daten/evidence.csv")
+# fitting a generalized linear model to use as baseline +
+names(evidence)
